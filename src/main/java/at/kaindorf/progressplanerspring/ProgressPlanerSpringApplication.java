64bfc828144ff705a6f8b738407f01fd02fa4acb
ranpad19 <ranpad19@htl-kaindorf.at>
2023-03-29 10:27:50 +0200

    Test

--- src/main/java/at/kaindorf/progressplanerspring/ProgressPlanerSpringApplication.java
+++ src/main/java/at/kaindorf/progressplanerspring/ProgressPlanerSpringApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProgressPlanerSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProgressPlanerSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressPlanerSpringApplication.class, args);
+        System.out.println("!!!!!!!");
+    }
 
 }
