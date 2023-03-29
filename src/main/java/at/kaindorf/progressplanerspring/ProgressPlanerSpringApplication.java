a7cd6183492f84f6abbc7e211b97903ac0903f4b
ranpad19 <ranpad19@htl-kaindorf.at>
2023-03-29 10:17:08 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/progressplanerspring/ProgressPlanerSpringApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.progressplanerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressPlanerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressPlanerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/kaindorf/progressplanerspring/ProgressPlanerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.progressplanerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressPlanerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
